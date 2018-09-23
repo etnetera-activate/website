fdbaef0a72d37f7efbd15914ab4de97511ab97db
skodizna <petr.skoda@etnetera.cz>
2018-09-24 00:15:25 +0200

    next big update

--- site.config.js
+++ site.config.js
@@ -1,11 +1,8 @@
-const data = require('./src/data/data');
-
 module.exports = {
 	site: {
 		title: 'Activate',
 		description: 'Micro Static Site Generator in Node.js',
-		basePath: process.env.NODE_ENV === 'production' ? '/test' : '',
-		data
+		basePath: process.env.NODE_ENV === 'production' ? '/test' : ''
 	},
 	build: {
 		outputPath: process.env.NODE_ENV === 'production' ? './docs' : './public'
