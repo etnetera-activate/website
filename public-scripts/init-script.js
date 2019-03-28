bc5e06e3973ff8950bad85d6695d6153827605ed
Tobias-Kucera <tobias.kucera@etnetera.cz>
2019-03-28 17:39:34 +0100

    GTM
    Přidán GTM snippet

--- public-scripts/init-script.js
+++ public-scripts/init-script.js
@@ -10,3 +10,12 @@ _paq.push(['enableLinkTracking']);
 	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
 	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
 })();
+
+
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W6M9MSR');</script>
+<!-- End Google Tag Manager -->
