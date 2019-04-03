4db2f592e3e35fef8c0809a5d0debac0698397d3
skodizna <petr.skoda@etnetera.cz>
2019-04-03 13:25:28 +0200

    gallery a uprava nasazeni gtm

--- public-scripts/init-script.js
+++ public-scripts/init-script.js
@@ -13,9 +13,9 @@ _paq.push(['enableLinkTracking']);
 
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W6M9MSR');</script>
+})(window,document,'script','dataLayer','GTM-W6M9MSR');
 <!-- End Google Tag Manager -->
