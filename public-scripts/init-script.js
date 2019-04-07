8eebedc4ecfc6d1bee863a7ea0d020f6c0d6bef3
Tobias-Kucera <tobias.kucera@etnetera.cz>
2019-04-07 05:17:13 +0200

    init scripts v1.6

--- public-scripts/init-script.js
+++ public-scripts/init-script.js
@@ -1,26 +1,91 @@
 var digitalData = digitalData || {};
+digitalData._log = [];
 
-(function(a, k, t, i, v, u, j, e, m) {
-// Async queue initialization
-	a[i] = a[i] || function() {
-		(a[i].q = a[i].q || []).push(arguments)
+// Main measure function
+measure = function (data) {
+
+	measure.notify (data);
+	measure.add (data);
+    measure.google (data);
+}
+
+// Notification to console
+measure.notify = function (data){
+	switch (data) {
+		case true:
+			localStorage.setItem("measureLog", true);
+			break;
+		case false:
+			localStorage.setItem("measureLog", false);
+			break;
+		default:
+			var measureLog = localStorage.getItem("measureLog");
+			if (measureLog == "true"){
+				console.group();
+				console.warn ("Measure function has received:\n" +
+					JSON.stringify(data, null, 2))
+				console.groupEnd();
+			}
 	}
-// Measure function
-	e = k.createElement(t),
-	m = k.getElementsByTagName(t)[0],
-	e.async = 1,
-	e.src="https://combinatronics.com/etnetera-activate/website/master/public-scripts/measureFunctions.js";
-	m.parentNode.insertBefore(e, m);
-// Google Snippet
-	e = k.createElement(t),
-	m = k.getElementsByTagName(t)[0],
-	e.async = 1,
-	e.src="https://combinatronics.com/etnetera-activate/website/master/public-scripts/googleSnippet.js";
-	m.parentNode.insertBefore(e, m);
-// Matomo Snippet
-	e = k.createElement(t),
-	m = k.getElementsByTagName(t)[0],
-	e.async = 1,
-	e.src="https://combinatronics.com/etnetera-activate/website/master/public-scripts/matomoSnippet.js";
-	m.parentNode.insertBefore(e, m);		
-})(window, document, "script", "measure")
+}
+
+// Add data to digitalData
+measure.add = function (data){
+	window.digitalData = measure.deepMerge (window.digitalData, data);
+	digitalData._log.push (data)
+}
+
+// Merge two objects
+measure.deepMerge = function (orgData, newData) {
+    var isArray = Array.isArray(newData);
+    var mergedData = isArray && newData || {};
+
+    if (!isArray) {
+      if (orgData && typeof orgData === 'object') {
+        Object.keys(orgData).forEach(function (key) {
+          mergedData[key] = orgData[key];
+        });
+      }
+      Object.keys(newData).forEach(function (key) {
+        if (typeof newData[key] !== 'object' || !newData[key]) {
+          mergedData[key] = newData[key];
+        } else {
+          if (!orgData[key]) {
+            mergedData[key] = newData[key];
+          } else {
+            mergedData[key] = measureInterface._deepMerge(orgData[key], newData[key]);
+          }
+        }
+      });
+    }
+    return mergedData;
+};
+
+// Measurement data transfer for Google
+measure.google = function (data){
+	if (data.action != undefined) {
+		data.event = data.action;
+	}
+    dataLayer.push (data)
+}
+
+
+// Google measurement snippet
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-W6M9MSR')
+
+// Matomo measurement snippet
+var _paq = window._paq || [];
+/* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+(function() {
+	var u="//matomo.activate.cz/";
+	_paq.push(['setTrackerUrl', u+'matomo.php']);
+	_paq.push(['setSiteId', '6']);
+	var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+	g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+})();
