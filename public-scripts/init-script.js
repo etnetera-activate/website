0e7e212f06e6fdea1943945336bdeae84e11268c
Tobias-Kucera <tobias.kucera@etnetera.cz>
2019-04-22 21:53:09 +0200

    consoleQuest v0.22

--- public-scripts/init-script.js
+++ public-scripts/init-script.js
@@ -44,7 +44,7 @@ measure.notify = function (data){
 
 			// For consoleQuest step 3
 			var consoleQuest3;
-			if (data.action=="slideShowMouseover" && data.slideShowPerson=="Lukáš Čech") {consoleQuest3 = "\n"+consoleQuestTexts[2]};
+			if (data.action=="slideShowMouseover" && data.slideShowPerson=="Lukáš Čech") {consoleQuest3 = "\n\n"+consoleQuestTexts[2]};
 
 			if (measureLog == "true"){
 				console.group();
@@ -129,7 +129,7 @@ consoleQuest.treasure = function(){
 var consoleQuestTexts = [
 	"Vítej poutníče,\nvypadáš unaveně. Jistě tě sem, do naší putiky U černé konzole, dovedla dlouhá a náročná cesta.\nBohužel není času nazbyt. Měl bych pro tebe úkol hodný hrdiny. Hrdiny, jež se zrodí nejvýše jednou za generaci. Uprostřed tajemného labyrintu jménem webová analytika se skrývá cenný poklad.\nJsi odvážného ducha a troufneš si ho hledat? Pokud ano, pokud se nebojíš, vyhledej místo známé jako datová vrstva ve které vládne mocný krutovládce Google. Budu tam na tebe čekat.\nHodně štěstí!",
 	"Zdravím tě,\nprvní překážky jsi se nezalekl a překonal jsi ji bez zaváhání. To velmi rád vidím.\nDokázal jsi tím, že jsi moudrý a odvážný, ale především že dokážeš uchopit tajemství, které ti hodlám předat.\nV království toho webu nevládne pouze Google, ale i jeho další bratři jako Adobe, Matomo a Snowplow. A každý z nich ale požaduje vybírat od svých poddaných data jiným způsobem. A co pak má dělat chudák bežná funkce, když musí odvést svůj datový desátek všem těmto mocnostem?\nVelký čaroděj pomocí svých mocných kouzel tedy přivedl k existenci funkci measure. Hodnou a dobrotivou funkci, která vybere data a přerozdělí je jednotlivým vládcům v podobách jaké požadují. Zavolej funkci measure.notify s parametrem true a udivíš, jaká data measure dostává.\nNyní musíš vyhledat samotného čaroděje dechberoucího Lukáše Čecha. Vyhledej jeho portrét a sleduj, co on předá spravedlivé funkci measure.\n/ Dokud nebude naimplementováno, tak zavolej measure({action:'slideShowMouseover', slideShowPerson:'Lukáš Čech'}) /",
-	"Kdo jsi a proč mě rušíš z mých meditací?\nPoklad? Ty si jdeš pro poklad? No to je velmi troufalé!\nNeřeknu ti, kde se poklad nachází, neb sám to nevím. Byl ukryt před dávnými časi, generace ho již nikdo nespatřil a mnozí již pochybují o jeho samé existenci.\nVím ale kde můžeš najít mapu k pokladu. Je skryta v pradávné jeskyni. Vchod do této jeskyně je schovaný pod hitem směřujícím do Matoma a otevřela se před okamžikem, když jsi rozpohyboval můj obraz.\nUtíkej, není čas ztrácet čas!",
+	"Kdo jsi a proč mě rušíš z mých meditací?\nPoklad? Ty si jdeš pro poklad? No to je velmi troufalé!\nNeřeknu ti, kde se poklad nachází, neb sám to nevím. Byl ukryt před dávnými časi, generace ho již nikdo nespatřil a mnozí již pochybují o jeho samé existenci.\nVím ale kde můžeš najít mapu k pokladu. Je skryta v pradávné jeskyni. Vchod do této jeskyně je schovaný pod hitem směřujícím do Matoma a otevřela se před okamžikem, když jsi rozpohyboval můj obraz.\nUtíkej, není čas ztrácet čas!\n/ Dokud nebude naimplementováno Matomo, tak hledej GA collect /",
 	"Poklad získá ten, kdo hrdě a beze strachu, nahlas pronese pradávná slova moci 'consoleQuest.treasure()'",
 	"Dokázal jsi to!\nPřekonal jsi všechna protiventví osudu, vyhledal mocného čaroděje a nalezl mapu dávno ztraceného pokladu.\nPokud by jsi se chtěl v budoucnu zůčastnit nějaké výpravy s námi, pošli holuba do našeho hradu 'my@activate.cz' a třeba se společně vypravíme k pohádkovému bohatství i nehynoucí slávě.\nNyní již ber svou zaslouženou odměnu hrdino. Ber dokud pokladnice nebude prázdná.",
 	"Pokladnice už je bohužel prázdná. U nás ve firemním Slacku ale zaručen dobrý vtip každy den."
@@ -137,8 +137,8 @@ var consoleQuestTexts = [
 
 // CSS Styles for notification to console
 var consoleQuestNotifyStyles = [
-	"border: 3px solid red",
-	"border-left: 4px dotted blue"
+	"background: linear-gradient(#092ae5, #030f56);border: 1px solid #3E0E02;color: white;display: block;text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3);box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset;line-height: 40px;text-align: center;font-weight: bold",
+	"border-left: 4px solid #092ae5"
 ];
 
 // Jokes for treasure
@@ -146,6 +146,10 @@ var consoleQuestLoot = [
 	'Farář zpovídá kostelníka.\nKostelník mluví, mluví a když skončí, farář povídá: "A to je všechno? Kdo mi chodí na mešní víno?"\nKostelník na to: "Tady je nějak špatně slyšet, pane faráři."\n"Nelži, kostelníku! Pán tě slyší!"\n"Tak si to pojďte zkusit, pane faráři."\nTak se vyměněj, kostelník si sedne na farářovo místo a farář se jde zpovídat. Kostelník povídá:\n"Tak co, pane faráři, kdo mi chodí za ženou?"\n"Máš pravdu, kostelníku. Je tu nějak špatně slyšet."',
 	'Pět lékařů je na lovu kachen. Praktický, dětský, psychiatr, chirurg a patolog.\nLetí kachna, praktický lékař zamíří a povídá: „Je to kachna? Možná ne, budu to muset prokonzultovat.“ Kachna mezitím uletí.\nLetí druhá kachna, dětský lékař zamíří: „Myslím, že je to kachna, ale co když má děti, musím to promyslet.“ Než se rozmyslí, kachna uletí.\nLetí další kachna, psychiatr zamíří: „Vím, že je to kachna, ale ví to i ona? Zná svoji identitu?“ Než to domyslí, kachna samozřejmě uletí.\nLetí další kachna, na řadě je chirurg, zamíří a kachnu střelí. Kachna spadne, chirurg se otočí na patologa a řekne: „Jděte se podívat, jestli to byla kachna.“',
 	'Přijde muž nešťastnou náhodou o přirození a navštíví plastického chirurga s tím,zda by mohl získat nástroj nový. Doktor v operaci nevidí žádný problém. Jen je nutné vybrat velikost.\n"Máme tři druhy" vysvětluje a názorně na modelech ukazuje.\n"Malého, kterého jste měl zřejmě vy, za sedm tisíc, většího za patnáct a největšího za dvaadvacet tisíc.\nNež se ale rozhodnete s konečnou platností pro jednu z variant, možná by bylo dobré se poradit s manželkou."\nMuž doma přednese všechny tři varianty.\nŽena se zamyslí a nakonec odpoví:\n"Já bych radši novou kuchyň." ',
+	'Zubař: "Chcete helium?"\nPacient: "A zmírní to bolest?"\nZubař: "Ne, ale když budete křičet, tak to bude srandovní".',
+	'Myslím si, že lidé by měli chodit bosí, protože je to opravdu mnohem zdravější. Například já, když se ráno vzbudím obutý, tak mě pak celý den ukrutně bolí hlava a ještě ke všemu mám hroznou žízeň..!',
+	'V módním obchodě.\nOna: "Tyhle boty bych si moc přála, miláčku, ale zapomněla jsem si doma peněženku..."\nOn: "A kolik stojí?"\nOna: "Tři a půl tisíce..."\nOn: *vytáhne peněženku*\nOna: *začne dychtit*\nOn: "Tady máš pade na autobus, zajeď si domů pro peněženku, počkám dole v hospodě."',
+	'Tchýně začala číst knihu "Exorcista". Začala, ale nedokončila - ta kniha je podle ní největší zlo, jaké kdy viděla. Tak velké, že šla a hodila ji z mola do moře.\n\nTak jsem koupil ještě jednu, pořádně ji namočil pod kohoutkem a dal jí ji do nočního stolku.\n\nMožná přijdu do pekla, ale určitě se budu smát ještě tam.',
 ];
 
 consoleQuest();
