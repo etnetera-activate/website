99296fa6855447a728c6392fe3f394da31d540ab
skodizna <petr.skoda@etnetera.cz>
2019-06-04 16:06:48 +0200

    uprava odesilani mailu (proti zneuziti a podpora pro hru), uprava formulare

--- functions/index.js
+++ functions/index.js
@@ -10,7 +10,7 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 // SLACK api webhook url
-const slackUrl = "https://hooks.slack.com/services/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";
+const slackUrl = "https://hooks.slack.com/services/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";
 // test slack url
 //const slackUrl = "https://hooks.slack.com/services/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";
 
@@ -25,13 +25,24 @@ exports.saveMessage = functions.https.onRequest((request, response) => {
 		const email = request.query.email;
 		const name = request.query.name; // snapshotId;
 		const date = new Date().toLocaleString();
+		let slackMessage = "";
+		let sendMail = true;
 
+		if ( name === "" || email === "") {
+			slackMessage = {
+				"text": "Problém u webového formuláře, jméno nebo e-mail jsou prázdné.  \n*Jméno:* " + name + "\n *e-mail:* " + email + "\n *datum:* " + date + "\n *text:* " + message,
+				"mrkdwn": true
+			};
+			sendMail = false;
 
+		} else {
+			slackMessage = {
+				"text": "*Nová zpráva z webového formuláře*\n*Jméno:* " + name + "\n *e-mail:* " + email + "\n *datum:* " + date + "\n *text:* " + message,
+				"mrkdwn": true
+			};
+		}
 		// SLACK hook
-		const slackMessage = {
-			"text": "*Nová zpráva z webového formuláře*\n*Jméno:* " + name + "\n *e-mail:* " + email + "\n *datum:* " + date + "\n *text:* " + message,
-			"mrkdwn": true
-		};
+
 
 		httpRequest.post({
 			url: slackUrl,
@@ -126,40 +137,121 @@ exports.saveMessage = functions.https.onRequest((request, response) => {
 			allowEmpty: true
 		});
 
-		httpRequest.post({
-			url: mailkitUrl,
-			headers: {
-				'Content-type': 'text/xml'
-			},
-			body: xml
-		}, function (error, response, body) {
-			if (error) {
-				console.log('mailkit Error: ' + error);
-			} else {
-				console.log('mailkit Done: ' + response);
-			}
-		});
-
-
-		// Firebase database
-		let submitData = {
-			message: message,
-			date: date,
-			email: email,
-			name: name
-		};
-
-		admin.database().ref('submits/' + Base64.encode(email)).push(submitData)
-			.then(snapshot => {
-				response.json({
-					'status': 'ok',
-				});
-				return 'tadaaaaa';
-
-			})
-			.catch(e => {
-				console.log('error' + e)
+		if (message === "DYCKYMAIL") {
+			xml = builder.create({
+				methodCall: {
+					methodName: 'mailkit.sendmail',
+					params: {
+						param: [
+							{
+								value: {
+									int: 118608497
+								}
+							}, {
+								value: {
+									string: 'da3bee408877b702fd62610de8d1c0a0'
+								}
+							}, {
+								value: {
+									int: 81773
+								}
+							}, {
+								value: {
+									int: 89333
+								}
+							}, {
+								value: {
+									struct: {
+										member: {
+											name: 'send_to',
+											value: {
+												string: email
+											}
+										}
+									}
+								}
+							}, {
+								value: {
+									struct: ''
+								}
+							}, {
+								value: {
+									struct: ''
+								}
+							}, {
+								value: {
+									struct: {
+										member: [
+											{
+												name: 'custom1',
+												value: {
+													string: Base64.encode(name)
+												}
+											}, {
+												name: 'custom2',
+												value: {
+													string: Base64.encode(email)
+												}
+											}, {
+												name: 'custom3',
+												value: {
+													string: Base64.encode(message)
+												}
+											}, {
+												name: 'custom4',
+												value: {
+													string: Base64.encode(date)
+												}
+											}
+										]
+									}
+								}
+							}
+						]
+					}
+				}
+			}).end({
+				allowEmpty: true
 			});
+		}
+
+		if (sendMail) {
+			httpRequest.post({
+				url: mailkitUrl,
+				headers: {
+					'Content-type': 'text/xml'
+				},
+				body: xml
+			}, function (error, response, body) {
+				if (error) {
+					console.log('mailkit Error: ' + error);
+				} else {
+					console.log('mailkit Done: ' + response);
+				}
+			});
+
+
+			// Firebase database
+			let submitData = {
+				message: message,
+				date: date,
+				email: email,
+				name: name
+			};
+
+			admin.database().ref('submits/' + Base64.encode(email)).push(submitData)
+				.then(snapshot => {
+					response.json({
+						'status': 'ok',
+					});
+					return 'tadaaaaa';
+
+				})
+				.catch(e => {
+					console.log('error' + e)
+				});
+		}
+
 
 	});
 });
