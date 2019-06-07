8a5344d3a0454ae9e715639f546c98c5808a9e6b
skodizna <petr.skoda@etnetera.cz>
2019-06-07 10:36:02 +0200

    console

--- public-scripts/init-script.js
+++ public-scripts/init-script.js
@@ -194,10 +194,7 @@ var consoleQuestTexts = [
 
 // CSS Styles for notification to console
 var consoleQuestNotifyStyles = [
-	"color: #1111c1; font-size: 130%; font-weight: 600",
-	"color: #b79000; font-size: 115%; font-weight: 600",
-	"background: linear-gradient(#092ae5, #030f56);border: 1px solid #3E0E02;color: white;display: block;text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3);box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset;line-height: 40px;text-align: center;font-weight: bold",
-	"border-left: 4px solid #092ae5"
+	"color: #01abdf; font-size: 130%; font-weight: 600; background-color: #000000"
 ];
 
 // Jokes for treasure
