9d99442a90ab5bf7b3aac8e9c40b410ee34bb829
lcech <ja@lukascech.cz>
2021-10-08 11:11:02 +0200

    Fix Cookie Consents

--- src/assets/js/init-script.js
+++ src/assets/js/init-script.js
@@ -6,7 +6,7 @@ digitalData._log = [];
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-W6M9MSR')
+})(window,document,'script','dataLayer','GTM-W6M9MSR');
 
 // Adobe measurement snippet
 /*
