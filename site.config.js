d05d99481c84d7d22ee4d7a55952e1b7e0674b3d
Klára Scholleová <klara@ksch.cz>
2021-12-22 18:29:14 +0100

    update site title from Activate to Etnetera A.

--- site.config.js
+++ site.config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	site: {
-		title: 'Activate ',
+		title: 'Etnetera Activate ',
 		description: 'Online konzultanti s vášní pro data',
 		basePath: process.env.NODE_ENV === 'production' ? '/test' : ''
 	},
